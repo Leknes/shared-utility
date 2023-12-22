3daf0b97491c91751e130058c3acffcc9bb31148
Leknes <leonhardsenkel@gmail.com>
2023-12-22 17:30:44 +0100

    Changed things

---
+++ Random/src/RandomUtility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Senkel.Utility.Random
+{
+    public static class RandomUtility
+    {
+#if !NET6_0_OR_GREATER
+        private static System.Random _shared = new System.Random();
+#endif
+
+        public static System.Random Shared
+        {
+            get
+            {  
+#if !NET6_0_OR_GREATER
+                return _shared;
+#else
+                return System.Random.Shared;
+#endif
+            }
+        }
+ 
+    }
+}
